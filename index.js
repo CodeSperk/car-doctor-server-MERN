36c47a85014b017709675c7cd03000b3135ad587
Mahbubur Rahman <inbx.mahbub@gmail.com>
2024-05-07 23:56:13 +0600

    added jwt to verify user before loading cart data

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+var jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -10,8 +12,35 @@ const port = process.env.PORT || 5000;
 
 
 //middleware 
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:5173'],
+  credentials: true
+}));
 app.use(express.json());
+app.use(cookieParser());
+
+// middlewares
+const logger = (req, res, next) => {
+  console.log('log info:', req.method, req.url);
+  console.log('This is cookies', req.cookies);
+  next();
+}
+
+const verifyToken = (req, res, next) => {
+  const token = req?.cookies?.token;
+  // console.log('token in the middleware', token);
+  //if no token found
+  if(!token){
+    return res.status(401).send({message: 'unauthorized access'});
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if(err){
+      return res.status(401).send({message: 'unauthorized access'})
+    }
+    req.user = decoded;
+    next();
+  })
+}
 
 
 const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster1.fi7xib0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1`;
@@ -30,6 +59,27 @@ async function run() {
 
     const serviceCollection = client.db("carDoctorDB").collection("services");
     const bookingsCollection = client.db("carDoctorDB").collection("bookings");
+
+//auth related  api
+app.post('/jwt', async(req, res) => {
+  const user = req.body;
+  console.log('user for token', user);
+  const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'});
+  res
+    .cookie('token', token, {
+      httpOnly: true,
+      secure: true,
+      sameSite:'none'
+    } )
+    .send({success: true})
+})
+
+// to clear cookie after logout
+app.post('/logout', async(req, res) => {
+  const user = req.body;
+  console.log('logging out ', user);
+  res.clearCookie('token', {maxAge: 0}).send({success:true})
+});
 
 // Services
     app.get("/services", async (req, res) => {
@@ -54,8 +104,14 @@ async function run() {
       res.send(result);
     })
       // Bookings
-      app.get("/bookings", async (req, res) => {
-        console.log(req.query);
+      app.get("/bookings", logger, verifyToken, async (req, res) => {
+        console.log(req.query.email);
+
+        console.log('token owner info', req.user);
+        if(req.user.email !== req.query.email){
+          return res.status(403).send({message: 'forbidden access'})
+        }
+
         let query = {};
         if(req.query?.email){
           query = {email: req.query.email}
@@ -84,9 +140,8 @@ async function run() {
     // await client.close();
   }
 }
+
 run().catch(console.dir);
-
-
 app.get("/", (req, res) => {
   res.send("Welcome to backend");
 })
